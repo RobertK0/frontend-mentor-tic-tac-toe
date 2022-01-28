119b51c6f77d9e6413231bb92dbd137b28ca1753
Zero0K <robykaurin@gmail.com>
2022-01-28 21:48:22 +0100

    Refactored entire code
    Put everything into classes,used private fields etc.,
    fixed scoreboard issues present when player
    selected o instead of x

--- script.js
+++ script.js
@@ -15,194 +15,209 @@ const markToggle = document.querySelector(".mark-toggle");
 const turnXSrc = "img/icon-x-gray.svg";
 const turnOSrc = "img/icon-o-gray.svg";
 
-let boardState, counter;
-let turn = true;
-
-let victoryFlag = false;
-let mark = "x";
-
-let scores = { x: 0, tie: 0, o: 0 };
-
 class App {
-  constructor() {}
+  #boardState;
+  #counter;
+  #turn = true;
+  #victoryFlag = false;
+  #mark = "x";
+  #scores = { x: 0, tie: 0, o: 0 };
+
+  constructor() {
+    container.addEventListener(
+      "click",
+      function (e) {
+        if (e.target.classList.contains("square")) this.#acceptInput(e.target);
+        if (e.target.classList.contains("restart-btn")) this.#init();
+      }.bind(this)
+    );
+
+    modal.addEventListener(
+      "click",
+      function (e) {
+        if (e.target.classList.contains("next-btn")) {
+          this.#init();
+          this.#toggleModal();
+        }
+        if (e.target.classList.contains("quit-btn")) this.#toggleModal();
+      }.bind(this)
+    );
+
+    newGame.addEventListener(
+      "click",
+      function (e) {
+        e.preventDefault();
+        if (e.target.classList.contains("new-btn")) {
+          this.#init();
+          document
+            .querySelector(".new-game-overlay")
+            .classList.toggle("hidden");
+        }
+        if (e.target.closest(".mark-toggle")) this.#toggleRadio(e);
+      }.bind(this)
+    );
+  }
+  ////Methods
+
+  #toggleRadio(e) {
+    let unchecked;
+    if (e.target.parentNode.querySelector(".mark")) {
+      markToggle.querySelectorAll(".mark").forEach((element) => {
+        if (!element.checked) unchecked = element;
+      });
+      unchecked.checked = !unchecked.checked;
+      this.#changeSelection(unchecked.value);
+    }
+  }
+
+  #changeSelection = function (selection) {
+    document
+      .querySelectorAll(".mark-icon")
+      .forEach((element) => element.classList.toggle("selected"));
+    this.#mark = selection;
+  };
+
+  #init() {
+    this.#boardState = [
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""],
+    ];
+
+    this.#counter = 0;
+
+    if (this.#mark === "x") {
+      this.#turn = true;
+    } else {
+      document.querySelector(".score-x .score-text").innerHTML = "X (CPU)";
+      document.querySelector(".score-o .score-text").innerHTML = "O (You)";
+      this.#turn = false;
+      const delay = (Math.random() * 1.5).toFixed(3);
+      setTimeout(() => this.#cpuInput(), delay * 1000);
+    }
+
+    this.#victoryFlag = false;
+
+    document
+      .querySelectorAll(".square")
+      .forEach((square) => (square.innerHTML = ""));
+  }
+
+  //done
+  #toggleModal() {
+    document.querySelector(".modal").classList.toggle("hidden");
+    document.querySelector(".overlay").classList.toggle("hidden");
+  }
+  //done
+  #acceptInput(square) {
+    if (
+      this.#turn &&
+      !this.#victoryFlag &&
+      this.#boardState[square.dataset.row][square.dataset.column].valueOf() ===
+        ""
+    ) {
+      this.#boardState[square.dataset.row][square.dataset.column] = `${
+        this.#mark
+      }`;
+      square.innerHTML = `<img src="img/icon-${this.#mark}.svg" alt="" />`;
+      this.#counter++;
+      this.#turn = !this.#turn;
+      this.#checkWin(`${this.#mark}`);
+      if (this.#mark.valueOf() === "x") {
+        turnImg.src = turnOSrc;
+      } else {
+        turnImg.src = turnXSrc;
+      }
+      const delay = (Math.random() * 1.5).toFixed(3);
+      setTimeout(() => this.#cpuInput(), delay * 1000);
+    }
+  }
+
+  #cpuInput() {
+    let row, column;
+    const cpuMark = this.#mark.valueOf() === "o" ? "x" : "o";
+    if (!this.#turn && this.#counter < 9 && !this.#victoryFlag) {
+      do {
+        row = Math.trunc(Math.random() * 3);
+        column = Math.trunc(Math.random() * 3);
+      } while (this.#boardState[row][column].valueOf() !== "");
+      this.#boardState[row][column] = `${cpuMark}`;
+      this.#counter++;
+      document.querySelector(
+        `[data-row="${row}"][data-column="${column}"]`
+      ).innerHTML = `<img src="img/icon-${cpuMark}.svg" alt="" />`;
+      this.#turn = !this.#turn;
+      if (this.#mark.valueOf() === "x") {
+        turnImg.src = turnXSrc;
+      } else {
+        turnImg.src = turnOSrc;
+      }
+      this.#checkWin(`${cpuMark}`);
+    }
+  }
+
+  #checkWin(player) {
+    function check(var1, var2, var3, var4 = 0, var5 = 1, var6 = 2) {
+      if (
+        this.#boardState[var4][var1].valueOf() !== "" &&
+        this.#boardState[var4][var1].valueOf() ===
+          this.#boardState[var5][var2].valueOf() &&
+        this.#boardState[var4][var1].valueOf() ===
+          this.#boardState[var6][var3].valueOf()
+      ) {
+        console.log(player, "victory");
+        this.#scores[player]++;
+
+        this.#victoryFlag = true;
+        this.#updateScoreboard();
+        this.#showWinner(player);
+      }
+    }
+
+    for (let i = 0; i < 3; i++) {
+      check.bind(this, 0, 1, 2, i, i, i)();
+
+      check.bind(this, i, i, i)();
+    }
+    check.bind(this, 0, 1, 2)();
+    check.bind(this, 2, 1, 0)();
+    console.log(this.#victoryFlag, this.#counter);
+    if (!this.#victoryFlag && this.#counter === 9) {
+      this.#scores.tie++;
+      this.#showWinner();
+    }
+    this.#updateScoreboard();
+  }
+
+  #updateScoreboard() {
+    for (const element in this.#scores) {
+      document.querySelector(`.score-value-${element}`).innerHTML =
+        this.#scores[element];
+    }
+  }
+
+  #showWinner(winner = "tie") {
+    const text = document.querySelector(".text-winner");
+    text.innerHTML = `It's a tie`;
+    if (winner !== "tie")
+      text.innerHTML = `<img src="img/icon-${winner}.svg" alt="" /> takes the round`;
+    let announcement = text.previousElementSibling;
+    if (winner === "x") {
+      text.style.color = "#31c3bd";
+      this.#mark === "x"
+        ? (announcement.innerHTML = "You won!")
+        : (announcement.innerHTML = "You lost...");
+    } else if (winner === "o") {
+      text.style.color = "#f2b137";
+      this.#mark === "o"
+        ? (announcement.innerHTML = "You won!")
+        : (announcement.innerHTML = "You lost...");
+    } else {
+      text.style.color = "#31c3bd";
+      announcement.innerHTML = `Awh...`;
+    }
+    this.#toggleModal();
+  }
 }
 
-////////Functions
-////Resets boardState variable and clears all html squares
-const clearBoard = function () {
-  init();
-};
-
-const acceptInput = function (square) {
-  if (
-    turn &&
-    !victoryFlag &&
-    boardState[square.dataset.row][square.dataset.column].valueOf() === ""
-  ) {
-    boardState[square.dataset.row][square.dataset.column] = `${mark}`;
-    square.innerHTML = `<img src="img/icon-${mark}.svg" alt="" />`;
-    console.table(boardState);
-    counter++;
-    turn = !turn;
-    checkWin(`${mark}`);
-    if (mark.valueOf() === "x") {
-      turnImg.src = turnOSrc;
-    } else {
-      turnImg.src = turnXSrc;
-    }
-    const delay = (Math.random() * 1.5).toFixed(3);
-    setTimeout(() => cpuInput(), delay * 1000);
-  }
-};
-
-const cpuInput = function () {
-  let row, column;
-  const cpuMark = mark.valueOf() === "o" ? "x" : "o";
-  if (!turn && counter < 9 && !victoryFlag) {
-    do {
-      row = Math.trunc(Math.random() * 3);
-      column = Math.trunc(Math.random() * 3);
-    } while (boardState[row][column].valueOf() !== "");
-    boardState[row][column] = `${cpuMark}`;
-    counter++;
-    document.querySelector(
-      `[data-row="${row}"][data-column="${column}"]`
-    ).innerHTML = `<img src="img/icon-${cpuMark}.svg" alt="" />`;
-    turn = !turn;
-    if (mark.valueOf() === "x") {
-      turnImg.src = turnXSrc;
-    } else {
-      turnImg.src = turnOSrc;
-    }
-    checkWin(`${cpuMark}`);
-  }
-};
-
-const checkWin = function (player) {
-  function check(var1, var2, var3, var4 = 0, var5 = 1, var6 = 2) {
-    if (
-      boardState[var4][var1].valueOf() !== "" &&
-      boardState[var4][var1].valueOf() === boardState[var5][var2].valueOf() &&
-      boardState[var4][var1].valueOf() === boardState[var6][var3].valueOf()
-    ) {
-      console.log(player, "victory");
-      scores[player]++;
-
-      victoryFlag = true;
-      updateScoreboard();
-      showWinner(player);
-    }
-  }
-  for (let i = 0; i < 3; i++) {
-    check(0, 1, 2, i, i, i);
-    check(i, i, i);
-  }
-  check(0, 1, 2);
-  check(2, 1, 0);
-  if (!victoryFlag && counter === 9) {
-    scores.tie++;
-    showWinner();
-  }
-  updateScoreboard();
-};
-
-const updateScoreboard = function () {
-  for (const element in scores) {
-    document.querySelector(`.score-value-${element}`).innerHTML =
-      scores[element];
-  }
-};
-
-const showWinner = function (winner = "tie") {
-  const text = document.querySelector(".text-winner");
-  text.innerHTML = `It's a tie`;
-  if (winner !== "tie")
-    text.innerHTML = `<img src="img/icon-${winner}.svg" alt="" /> takes the round`;
-  let announcement = text.previousElementSibling;
-  if (winner === "x") {
-    text.style.color = "#31c3bd";
-    mark === "x"
-      ? (announcement.innerHTML = "You won!")
-      : (announcement.innerHTML = "You lost...");
-  } else if (winner === "o") {
-    text.style.color = "#f2b137";
-    mark === "o"
-      ? (announcement.innerHTML = "You won!")
-      : (announcement.innerHTML = "You lost...");
-  } else {
-    text.style.color = "#31c3bd";
-    announcement.innerHTML = `Awh...`;
-  }
-  toggleModal();
-};
-
-const toggleModal = function () {
-  document.querySelector(".modal").classList.toggle("hidden");
-  document.querySelector(".overlay").classList.toggle("hidden");
-};
-
-const changeSelection = function (selection) {
-  document
-    .querySelectorAll(".mark-icon")
-    .forEach((element) => element.classList.toggle("selected"));
-  console.log(selection);
-  mark = selection;
-};
-
-const init = function () {
-  boardState = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""],
-  ];
-
-  counter = 0;
-  if (mark.valueOf() === "x") {
-    turn = true;
-  } else {
-    turn = false;
-    const delay = (Math.random() * 1.5).toFixed(3);
-    setTimeout(() => cpuInput(), delay * 1000);
-  }
-
-  victoryFlag = false;
-
-  document
-    .querySelectorAll(".square")
-    .forEach((square) => (square.innerHTML = ""));
-};
-
-const toggleRadio = function (e) {
-  let unchecked;
-  if (e.target.parentNode.querySelector(".mark")) {
-    markToggle.querySelectorAll(".mark").forEach((element) => {
-      if (!element.checked) unchecked = element;
-    });
-    unchecked.checked = !unchecked.checked;
-    changeSelection(unchecked.value);
-  }
-};
-
-////////Listeners
-
-container.addEventListener("click", function (e) {
-  if (e.target.classList.contains("square")) acceptInput(e.target);
-  if (e.target.classList.contains("restart-btn")) init();
-});
-
-modal.addEventListener("click", function (e) {
-  if (e.target.classList.contains("next-btn")) {
-    init();
-    toggleModal();
-  }
-  if (e.target.classList.contains("quit-btn")) toggleModal();
-});
-
-newGame.addEventListener("click", function (e) {
-  e.preventDefault();
-  if (e.target.classList.contains("new-btn")) {
-    init();
-    document.querySelector(".new-game-overlay").classList.toggle("hidden");
-  }
-  if (e.target.closest(".mark-toggle")) toggleRadio(e);
-});
+const app = new App();
